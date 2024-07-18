c80e1134b5834a92373b6d4b29fff63e34bd6cc8
artemkot <artem.alex.kot@gmail.com>
2024-07-18 13:16:36 +0200

    Updated scheduler

--- src/main/java/org/example/trading/service/MoneyTransferOrderScheduler.java
+++ src/main/java/org/example/trading/service/MoneyTransferOrderScheduler.java
@@ -1,18 +1,18 @@
-package org.example.cat.service;
+package org.example.trading.service;
 
 import io.quarkus.scheduler.Scheduled;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
-import org.example.cat.model.FoodTransferOrder;
+import org.example.trading.model.CashTransferOrder;
 
 import java.util.Map;
 import java.util.Set;
 
 @ApplicationScoped
-public class FoodTransferOrderScheduler {
+public class MoneyTransferOrderScheduler {
 
     @Inject
-    FoodTransferService foodTransferService;
+    MoneyTransferService moneyTransferService;
 
     @Scheduled(every = "20s")
     public void updateOrderStatus() {
@@ -21,9 +21,9 @@ public class FoodTransferOrderScheduler {
     }
 
     private void updateOrdersStatus(String environment) {
-        Set<Map.Entry<String, FoodTransferOrder>> orders = foodTransferService.getAllOrders(environment);
-        for (Map.Entry<String, FoodTransferOrder> entry : orders) {
-            FoodTransferOrder order = entry.getValue();
+        Set<Map.Entry<String, CashTransferOrder>> orders = moneyTransferService.getAllOrders(environment);
+        for (Map.Entry<String, CashTransferOrder> entry : orders) {
+            CashTransferOrder order = entry.getValue();
             if ("new".equals(order.getStatus())) {
                 order.setStatus("in_progress");
             }
